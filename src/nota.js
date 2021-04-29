6a2129e5bbdadbe20a563901740ef15742d9ddb8
Ruales <jdruales4@misena.edu.co>
2021-04-29 16:37:30 -0500

    segundo cambio

--- src/nota.js
+++ src/nota.js
@@ -1,1 +1,41 @@
-let nombre="Hola mundo Colombia";
+class materia{
+    constructor(nombreM){
+        this.nombreM=nombreM;
+        
+    }
+    mostrarNombre(){
+        return `
+         <b> nombre:</b>  ${this.nombreM}<br /> 
+         
+         <hr />
+        `;
+    }
+ 
+ }
+ 
+ const area= new materia("matematicas");
+ document.write(area.mostrarNombre());
+ 
+ 
+ class areas extends materia{
+     constructor(nombreA,nombreM){
+         super(nombreM)
+        this.nombreA=nombreA;
+     }
+    
+     mostrarinfo(){
+        return `
+         <b> nombre de la materia:</b>  ${this.nombreM}<br /> 
+         <b> nombre de la area:</b>  ${this.nombreA}<br /> 
+         <hr />
+        `;
+    }
+      
+    }
+    const materias= new areas("algoritmia","matematicas");
+    document.write(materias.mostrarinfo());
+ 
+ 
+ 
+ 
+ 
